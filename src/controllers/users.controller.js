04eb4d7ea1278c2b86f43c0131334dfb72d8a436
Tedros Teshome <tedrhanna@gmail.com>
2026-02-20 22:58:46 +0300

    feat: function to create new usernusing just email and name

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -16,3 +16,22 @@ const getUserById = (req, res) => {
 
     res.json(user);
 };
+
+// Create new user
+const createUser = (req, res) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({ message: 'Name and email are required' });
+    }
+
+    const newUser = {
+        id: users.length ? users[users.length - 1].id + 1 : 1,
+        name,
+        email
+    };
+
+    users.push(newUser);
+
+    res.status(201).json(newUser);
+};
