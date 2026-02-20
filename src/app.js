1ea65329a558d34709238406495afd66e4ce714e
Tedros Teshome <tedrhanna@gmail.com>
2026-02-20 22:34:04 +0300

    feat: define and initialize express and different endpoints needs

--- src/app.js
+++ src/app.js
@@ -2,13 +2,27 @@ const express = require('express');
 
 const app = express();
 
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/users', (req, res) => {
+  res.json([]);
 });
+
+app.get('/users/:id', (req, res) => {
+  res.json({ id: req.params.id });
+});
+
+app.post('/users', (req, res) => {
+  res.json(req.body);
+});
+
+app.put('/users/:id', (req, res) => {
+  res.json(req.body);
+});
+
+app.delete('/users/:id', (req, res) => {
+  res.json({ id: req.params.id });
+});
+
+app.listen(3000);
+
 
 module.exports = app;
