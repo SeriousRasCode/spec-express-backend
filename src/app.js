56b9a4aab5ed2ee4dd0f503853851f2a3321709e
Tedros Teshome <tedrhanna@gmail.com>
2026-02-20 23:09:19 +0300

    feat: register middleware in app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ app.use(express.json());
 const usersRoutes = require('./routes/users.routes');
 app.use('/users', usersRoutes);
 
+const errorHandler = require('./middleware/error.middleware');
+app.use(errorHandler);
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
