416e3118ffde09aa779ce229c0b9cc5b794c3d26
Tedros Teshome <tedrhanna@gmail.com>
2026-02-20 22:56:58 +0300

    feat:  create the logic to get users by id

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,3 +4,15 @@ let users = [];
 const getAllUsers = (req, res) => {
     res.json(users);
 };
+
+// Get user by ID
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+};
