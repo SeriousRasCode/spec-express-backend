1ed32b55b249a98296f33fcd249a185e80a3a2f6
Tedros Teshome <tedrhanna@gmail.com>
2026-02-20 22:07:34 +0300

    basic mplementation for modification

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = app;
