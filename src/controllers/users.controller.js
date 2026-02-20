f15ef981c5a2492830bd428a6f46cfb55b57d95d
Tedros Teshome <tedrhanna@gmail.com>
2026-02-20 23:08:40 +0300

    refine for use of error middleware

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,67 +1,93 @@
 let users = [];
 
-// Get all users
+// get all users
 const getAllUsers = (req, res) => {
     res.json(users);
 };
 
-// Get user by ID
-const getUserById = (req, res) => {
-    const id = parseInt(req.params.id);
-    const user = users.find(user => user.id === id);
+// get user by ID
+const getUserById = (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const user = users.find(user => user.id === id);
 
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        if (!user) {
+            const error = new Error('User not found');
+            error.status = 404;
+            return next(error);
+        }
+
+        res.json(user);
+    } catch (err) {
+        next(err);
     }
-
-    res.json(user);
 };
 
-// Create new user
-const createUser = (req, res) => {
-    const { name, email } = req.body;
+// create user
+const createUser = (req, res, next) => {
+    try {
+        const { name, email } = req.body;
 
-    if (!name || !email) {
-        return res.status(400).json({ message: 'Name and email are required' });
+        if (!name || !email) {
+            const error = new Error('Name and email are required');
+            error.status = 400;
+            return next(error);
+        }
+
+        const newUser = {
+            id: users.length ? users[users.length - 1].id + 1 : 1,
+            name,
+            email
+        };
+
+        users.push(newUser);
+
+        res.status(201).json(newUser);
+    } catch (err) {
+        next(err);
     }
-
-    const newUser = {
-        id: users.length ? users[users.length - 1].id + 1 : 1,
-        name,
-        email
-    };
-
-    users.push(newUser);
-    res.status(201).json(newUser);
 };
 
-// Update user
-const updateUser = (req, res) => {
-    const id = parseInt(req.params.id);
-    const { name, email } = req.body;
-    const user = users.find(user => user.id === id);
+// update user
+const updateUser = (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const { name, email } = req.body;
+        const user = users.find(user => user.id === id);
 
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        if (!user) {
+            const error = new Error('User not found');
+            error.status = 404;
+            return next(error);
+        }
+
+        if (name) user.name = name;
+        if (email) user.email = email;
+
+        res.json(user);
+    } catch (err) {
+        next(err);
     }
-
-    if (name) user.name = name;
-    if (email) user.email = email;
-
-    res.json(user);
 };
 
-// Delete user
-const deleteUser = (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = users.findIndex(user => user.id === id);
+// delete user
+const deleteUser = (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const index = users.findIndex(user => user.id === id);
 
-    if (index === -1) {
-        return res.status(404).json({ message: 'User not found' });
+        if (index === -1) {
+            const error = new Error('User not found');
+            error.status = 404;
+            return next(error);
+        }
+
+        const deletedUser = users.splice(index, 1);
+
+        res.json(deletedUser[0]);
+    } catch (err) {
+        next(err);
     }
-
-    const deletedUser = users.splice(index, 1);
-    res.json(deletedUser[0]);
 };
 
 module.exports = {
